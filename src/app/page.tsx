d3a824193799a2615719d0477659625e57da639a
Stasn13 <stasn139@gmail.com>
2025-05-28 22:37:49 +0100

    update card borders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <h2 className="text-white">Stanislav Gavrylenko</h2>
         </div>
         <div className="flex gap-2 w-full">
-          <Card className="bg-background-card-1 border-none flex-1 border-xl rounded-xl">
+          <Card className="bg-background-card-1 border-none flex-1 border-xl rounded-4xl">
             <CardHeader>
               <CardTitle>skills</CardTitle>
             </CardHeader>
@@ -31,14 +31,14 @@ export default function Home() {
           </Card>
           <div className="flex flex-col gap-2 flex-1">
             <div className="flex gap-2">
-              <Card className="bg-background-card-2 border-none flex-1">
+              <Card className="bg-background-card-2 border-none flex-1 rounded-4xl">
                 works(projects)
               </Card>
-              <Card className="bg-background-card-3 border-none flex-1 text-white">
+              <Card className="bg-background-card-3 border-none flex-1 text-white rounded-4xl">
                 here we go
               </Card>
             </div>
-            <Card className="bg-background-card-4 border-none flex-2">
+            <Card className="bg-background-card-4 border-none flex-2 rounded-4xl">
               here we go
             </Card>
           </div>
