add171d54ed2d1cb06c6d98b343e9167f9a81acd
Stasn13 <stasn139@gmail.com>
2025-05-28 22:36:34 +0100

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,48 @@
-import Image from "next/image";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div className="p-4">
-      <section>
-        <h1 className="text-white">Frontend Developer</h1>
-        <h2 className="text-white">Stanislav Gavrylenko</h2>
-        <Card className="bg-background-blue">skills</Card>
-        <Card className="bg-white">works</Card>
-        <Card>here we go</Card>
-        <Card>here we go</Card>
+      <section className="flex flex-col h-[100vh] justify-between">
+        <div>
+          <h1 className="text-white font-bold text-4xl">Frontend Developer</h1>
+          <h2 className="text-white">Stanislav Gavrylenko</h2>
+        </div>
+        <div className="flex gap-2 w-full">
+          <Card className="bg-background-card-1 border-none flex-1 border-xl rounded-xl">
+            <CardHeader>
+              <CardTitle>skills</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+                <li>next</li>
+                <li>tailwind</li>
+                <li>typescript</li>
+                <li>git</li>
+                <li>github</li>
+                <li>figma</li>
+                <li>adobe xd</li>
+              </ul>
+            </CardContent>
+          </Card>
+          <div className="flex flex-col gap-2 flex-1">
+            <div className="flex gap-2">
+              <Card className="bg-background-card-2 border-none flex-1">
+                works(projects)
+              </Card>
+              <Card className="bg-background-card-3 border-none flex-1 text-white">
+                here we go
+              </Card>
+            </div>
+            <Card className="bg-background-card-4 border-none flex-2">
+              here we go
+            </Card>
+          </div>
+        </div>
       </section>
     </div>
   );
