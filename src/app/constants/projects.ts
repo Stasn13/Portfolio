e37288bdda1b16a7898118f5284d2dc60918bc7f
Stasn13 <stasn139@gmail.com>
2025-06-05 23:47:30 +0100

    add projects list

---
+++ src/app/constants/projects.ts
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+        name: "Cardosis",
+        description: "Description of project 1",
+        image: "https://via.placeholder.com/150",
+        link: "https://github.com",
+    },
+    {
+        name: "BacnkWatch",
+        description: "Description of project 2",
+        image: "https://via.placeholder.com/150",
+        link: "https://github.com",
+    },
+]
