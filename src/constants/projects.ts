7a8fb4f176db35ccb74e4b2c93e28043308c0852
Stasn13 <stasn139@gmail.com>
2025-06-11 22:50:25 +0100

    update projects data

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,14 +1,21 @@
+import cardosisImg from "../assets/img/cardosis.png"
+import bankwatchImg from "../assets/img/bankwatch.png"
+
 export const projects = [
     {
         name: "Cardosis",
         description: "Description of project 1",
         image: "https://via.placeholder.com/150",
         link: "https://github.com",
+        img: cardosisImg,
+        desc: "A Decentralized Solution for Attesting Financial Activities in crypto banks"
     },
     {
         name: "BankWatch",
         description: "Description of project 2",
         image: "https://via.placeholder.com/150",
         link: "https://github.com",
+        img: bankwatchImg,
+        desc: "Description of project 2"
     },
 ]
