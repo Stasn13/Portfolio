0dc5102fb8b061f169e89ba875e40d1a2ac7111d
Stasn13 <stasn139@gmail.com>
2025-06-11 22:23:55 +0100

    connect fonts

---
+++ src/lib/fonts.ts
@@ -0,0 +1,88 @@
+import localFont from 'next/font/local';
+
+export const steradian = localFont({
+  src: [
+    {
+      path: '../assets/fonts/SteradianTRIAL-Th.otf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-ThIt.otf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-UltLt.otf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-UltLtIt.otf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-XLt.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-XLtIt.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-Lt.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-LtIt.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-Rg.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-RgIt.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-Md.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-MdIt.otf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-Bd.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-BdIt.otf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-Blk.otf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SteradianTRIAL-BlkIt.otf',
+      weight: '800',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-steradian',
+  display: 'swap',
+});
